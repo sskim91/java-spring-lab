7c18c343be8ea4d063e310ea411533bc9037d4b0
sskim91 <tjdtjq91@gmail.com>
2020-10-21 17:57:18 +0900

    feat: resp-api 샘플프로젝트 생성

---
+++ sample-rest-api/src/main/java/com/sskim/restapi/domain/UserVO.java
@@ -0,0 +1,19 @@
+package com.sskim.restapi.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author sskim
+ */
+@Getter
+@Setter
+@ToString
+public class UserVO {
+
+    private String name;
+    private int age;
+    private String address;
+
+}
