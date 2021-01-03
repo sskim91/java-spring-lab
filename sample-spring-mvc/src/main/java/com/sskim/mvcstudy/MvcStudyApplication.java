52e5f4f6fee99757fd0ec8f6c7fa1249c1542c2e
sskim91 <tjdtjq91@gmail.com>
2021-01-03 22:15:07 +0900

    Initial Commit

---
+++ sample-spring-mvc/src/main/java/com/sskim/mvcstudy/MvcStudyApplication.java
@@ -0,0 +1,13 @@
+package com.sskim.mvcstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcStudyApplication.class, args);
+    }
+
+}
---
+++ sample-spring-mvc/src/test/java/com/sskim/mvcstudy/MvcStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sskim.mvcstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
