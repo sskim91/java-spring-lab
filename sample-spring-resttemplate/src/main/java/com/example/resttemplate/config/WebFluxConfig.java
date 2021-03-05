97d1b3ba0942b9b854ab2c341017d506d74c936b
sskim91 <tjdtjq91@gmail.com>
2021-03-05 14:38:59 +0900

    Feat: WebFlux WebClient 마구잡이 쳐보기

--- sample-spring-resttemplate/src/main/java/com/example/resttemplate/config/WebFluxConfig.java
+++ sample-spring-resttemplate/src/main/java/com/example/resttemplate/config/WebFluxConfig.java
@@ -1,12 +1,42 @@
 package com.example.resttemplate.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 /**
  * @author sskim
  */
 @Configuration
+@Slf4j
 public class WebFluxConfig implements WebFluxConfigurer {
 
+    @Bean
+    public WebClient webClient() {
+        WebClient webClient = WebClient.builder()
+                .baseUrl("http://www.kobis.or.kr")
+                .filter(ExchangeFilterFunction.ofRequestProcessor(
+                        clientRequest -> {
+                            log.debug("Request: {} {}", clientRequest.method(), clientRequest.url());
+                            clientRequest.headers()
+                                    .forEach((name, values) -> values.forEach(value -> log.debug("{} : {}", name, value)));
+                            return Mono.just(clientRequest);
+                        }
+                ))
+                .filter(ExchangeFilterFunction.ofResponseProcessor(
+                        clientResponse -> {
+                            clientResponse.headers()
+                                    .asHttpHeaders()
+                                    .forEach((name, values) ->
+                                            values.forEach(value -> log.debug("{} : {}", name, value)));
+                            return Mono.just(clientResponse);
+                        }
+                ))
+                .build();
+        return webClient;
+    }
 }
--- sample-spring-resttemplate/src/main/java/com/example/resttemplate/controller/WebClientController.java
+++ sample-spring-resttemplate/src/main/java/com/example/resttemplate/controller/WebClientController.java
@@ -1,11 +1,45 @@
 package com.example.resttemplate.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * @author sskim
  */
 @RestController
+@RequiredArgsConstructor
 public class WebClientController {
 
+    private final WebClient webClient;
+
+    @GetMapping("/webClientRequest")
+    public ResponseEntity webClientRequest() {
+
+        List<Object> collect = webClient
+                .mutate()
+                .build()
+                .get()
+                .uri(uriBuilder -> {
+                    uriBuilder.path("/kobisopenapi/webservice/rest/people/searchPeopleInfo.json")
+                            .queryParam("key", "799f23355d82c9c14f5e42bbf848130b")
+                            .queryParam("peopleCd", "10055626");
+                    return uriBuilder.build();
+                })
+                .retrieve()
+                .bodyToMono(Object.class)
+                .flux()
+                .toStream()
+                .collect(toList());
+
+        System.out.println("movieDTOMono = " + collect);
+
+        return ResponseEntity.ok(collect);
+    }
 }
